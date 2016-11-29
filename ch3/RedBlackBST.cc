5360c0580b7b0ddb6129036eb9c48166d54cdb73
JayfonLin <linjiafang33@163.com>
2016-11-29 23:53:32 +0800

    red-black tree

--- ch3/RedBlackBST.cc
+++ ch3/RedBlackBST.cc
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <cstdlib>
 #include <string>
+#include <stack>
+
 
 template <class Key, class Value>
 class RedBlackBST
@@ -32,6 +34,85 @@ public:
 		return isBalanced(root_, black);
 	}
 
+	bool is23(){
+		Node* x = root_;
+		std::stack<Node*> s;
+		if (x != NULL){
+			s.push(x);
+		}
+
+		while(!s.empty()){
+			x = s.top();
+			s.pop();
+
+			if (isRed(x->right_)){
+				return false;
+			}
+
+			if (x != root_ && isRed(x) && isRed(x->left_)){
+				return false;
+			}
+
+			if (x->left_ != NULL){
+				s.push(x->left_);
+			}
+
+			if (x->right_ != NULL){
+				s.push(x->right_);
+			}
+		}
+
+		return true;
+	}
+
+	bool isBST(){
+		return isBST(root_, NULL, NULL);
+	}
+
+	bool isRedBlackBST(){
+		return isBST() && is23() && isBalanced(); 
+	}
+
+	void deleteMin(){
+		if (isEmpty()){
+			return;
+		}
+
+		if (!isRed(root_->left_) && !isRed(root_->right_)) {
+			root_->color = kRed;
+		}
+
+		root_ = deleteMin(root_);
+		if (!isEmpty()) {
+			root_->color = kBlack;
+		}
+
+		assert(isRedBlackBST());
+	}
+
+	bool isEmpty(){
+		return root_ == NULL;
+	}
+
+	bool contains(const Key& key){
+		return get(key) != NULL;
+	}
+
+	void mdelete(const Key& key){
+		if (!contains(key)){
+			return;
+		}
+
+		if (!isRed(root_->left_) && !isRed(root_->right_)){
+			root_->color_ = kRed;
+		}
+
+		root_ = mdelete(root_, key);
+		if (!isEmpty()){
+			root_->color_ = kBlack;
+		}
+	}
+
 	
 
 private:
@@ -51,6 +132,95 @@ private:
 			right_ = NULL;
 		}
 	};
+
+	Node* mdelete(Node* h, const Key& key){
+		assert( get(h, key) != NULL );
+
+		if (key < h->key_){
+			if (!isRed(h->left_) && !isRed(h->left_->left_)){
+				h = moveRedLeft(h);
+			}
+			h->left_ = mdelete(h->left_, key);
+		}else{
+			if (isRed(h->left_)){
+				h = rotateRight(h);
+			}
+			if (key == h->key_ && h->right_ == NULL){
+				delete h;
+				return NULL;
+			}
+			if (!isRed(h->right_) && !isRed(h->right_->left_)){
+				h = moveRedRight(h);
+			}
+			if (key == h->key_){
+				Node* x = min(h->right_);
+				h->key_ = x->key_;
+				h->value_ = x->value_;
+
+				h->right_ = deleteMin(h->right_);
+			}
+			else{
+				h->right_ = mdelete(h->right_, key);
+			}
+		}
+
+		return balance(h);
+	}
+
+	Node* deleteMin(Node* h){
+		if (h->left_ == NULL){
+			delete h;
+			return NULL;
+		}
+
+		if (!isRed(h->left_) && !isRed(h->left_->left_)){
+			h = moveRedLeft(h);
+		}
+
+		h->left_ = deleteMin(h->left_);
+		return balance(h);
+	}
+
+	Node* moveRedLeft(Node* h){
+		assert(h != NULL);
+		assert( isRed(h) && !isRed(h->left_) && !isRed(h->left_->left_) );
+
+		flipColors(h);
+		if (isRed(h->right_->left_)){
+			h->right_ = rotateRight(h->right_);
+			h = rotateLeft(h);
+			flipColors(h);
+		}
+
+		return h;
+	}
+
+	Node* moveRedRight(Node* h){
+		assert(h != NULL);
+		assert( isRed(h) && !isRed(h->right_) && !isRed(h->right_->left_));
+
+		flipColors(h);
+		if (isRed(h->left_->left_)){
+			h = rotateRight(h);
+			flipColors(h);
+		}
+
+		return h;
+	}
+
+	bool isBST(Node* x, const Key* min, const Key* max){
+		if (x == NULL) return true;
+
+		if (min != NULL && *min >= x->key_){
+			return false;
+		}
+
+		if (max != NULL && *max <= x->key_){
+			return false;
+		}
+
+		return isBST(x->left_, min, &x->key_) && isBST(x->right_, &x->key_, max);
+	}
 
 	bool isBalanced(Node* x, int black){
 		if (x == NULL) return black == 0;
@@ -88,6 +258,24 @@ private:
 		return h;
 	}
 
+	Node* balance(Node* h){
+		assert(h != NULL);
+
+		if (isRed(h->right_)){
+			h = rotateLeft(h);
+		}
+
+		if (isRed(h->left_) && isRed(h->left_->left_)){
+			h = rotateRight(h);
+		}
+
+		if (isRed(h->left_) && isRed(h->right_)){
+			flipColors(h);
+		}
+
+		return h;
+	}
+	
 	bool isRed(Node* x){
 		if (x == NULL) return false;
 
@@ -117,11 +305,13 @@ private:
 	}
 
 	void flipColors(Node* h){
-		assert( !isRed(h) && isRed(h->left_) && isRed(h->right_) );
+		assert( (h != NULL) && (h->left_ != NULL) && (h->right_ != NULL) );
+		assert( (!isRed(h) && isRed(h->left_) && isRed(h->right_)) 
+			|| (isRed(h) && !isRed(h->left_) && !isRed(h->right_)) );
 
-		h->color_ = kRed;
-		h->left_->color_ = kBlack;
-		h->right_->color_ = kBlack;
+		h->color_ = !h->color_;
+		h->left_->color_ = !h->left_->color_;
+		h->right_->color_ = !h->right_->color_;
 	}
 
 
@@ -147,6 +337,7 @@ int main(int argc, const char* argv[])
 	}
 
 	printf("is balanced tree:%d\n", tree.isBalanced());
+	printf("is red-black binary tree:%d\n", tree.isRedBlackBST());
 
 	char* f = const_cast<char*>(argv[1]);
 	std::string test_str(f);
